317b5305f1b7099f00e34eb313ecd0e121955306
AndreFRSales <andrefernandes.sales@gmail.com>
2016-06-11 16:58:11 -0300

    Implementing ActionSheet without actions

---
+++ ContatosIP67/GerenciadorDeAcoes.h
@@ -0,0 +1,21 @@
+//
+//  GerenciadorDeAcoes.h
+//  ContatosIP67
+//
+//  Created by ios5994 on 11/06/16.
+//  Copyright © 2016 Caelum. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+#import "Contato.h"
+
+
+@interface GerenciadorDeAcoes : NSObject<UIActionSheetDelegate>
+
+@property Contato *contato;
+@property UIViewController *controller;
+-(id)initWithContato:(Contato *)contato;
+-(void)acoesDoController:(UIViewController *)controller;
+
+@end
